40ad0cf839bac343c78fad2a2feacdc8575781fa
vishalchauhan91196 <vishalchauhan91196@users.noreply.github.com>
2021-03-31 16:33:54 +0530

    refactor: added funtranslations api

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const txtInput = document.querySelector("#txt-input");
 const btnTranslate = document.querySelector(".btn-translate");
 const txtOutput = document.querySelector(".output");
 
+const getURL = function (text) {
+  return "https://api.funtranslations.com/translate/minion.json?text=" + text;
+};
+
+const errorHandler = function (error) {
+  alert("Something went wrong.");
+  console.log(error.message);
+};
+
 const translator = function () {
-  txtOutput.innerText = "Welcome, " + txtInput.value;
+  fetch(getURL(txtInput.value))
+    .then((response) => response.json())
+    .then((data) => (txtOutput.innerText = data.contents.translated))
+    .catch(errorHandler);
 };
 
 btnTranslate.addEventListener("click", translator);
